d38e7077a66f80bf262047a168a7363c2800de15
LukaszZachariasz <lukaszzachariasz.contactme@gmail.com>
2020-06-11 13:01:20 +0200

    init frontend

--- bittorrent-simulator/src/app/app.module.ts
+++ bittorrent-simulator/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client-management/client/client.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClientManagementComponent } from './components/client-management/client-management.component';
+import { TrackerManagementComponent } from './components/tracker-management/tracker-management.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientListComponent } from './components/client-management/client-list/client-list.component';
+import {FlexModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    HeaderComponent,
+    ClientManagementComponent,
+    TrackerManagementComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ bittorrent-simulator/src/app/components/tracker-management/tracker-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker-management',
+  templateUrl: './tracker-management.component.html',
+  styleUrls: ['./tracker-management.component.css']
+})
+export class TrackerManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
