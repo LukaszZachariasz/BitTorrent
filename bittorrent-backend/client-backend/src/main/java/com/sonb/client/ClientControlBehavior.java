c247ee2e75810c60106ddcf8f7465a28af736004
LukaszZachariasz <lukaszzachariasz.contactme@gmail.com>
2020-06-14 21:47:55 +0200

    Deny and allow access to file for others clients, code refactor, tracker/client small bugfix

--- bittorrent-backend/client-backend/src/main/java/com/sonb/client/ClientControlBehavior.java
+++ bittorrent-backend/client-backend/src/main/java/com/sonb/client/ClientControlBehavior.java
@@ -8,6 +8,8 @@ public class ClientControlBehavior {
 
     private static int simulateDownloadDelay = 1000;
 
+    private static int maxRetiresToDownloadPart = 3;
+
     public static void simulateDownloadDelay() {
         try {
             Thread.sleep(simulateDownloadDelay);
@@ -22,5 +24,13 @@ public class ClientControlBehavior {
 
     public static void setSimulateDownloadDelay(int simulateDownloadDelay) {
         ClientControlBehavior.simulateDownloadDelay = simulateDownloadDelay;
+    }
+
+    public static int getMaxRetiresToDownloadPart() {
+        return maxRetiresToDownloadPart;
+    }
+
+    public static void setMaxRetiresToDownloadPart(int maxRetiresToDownloadPart) {
+        ClientControlBehavior.maxRetiresToDownloadPart = maxRetiresToDownloadPart;
     }
 }
--- bittorrent-simulator/src/app/constants/urls/client-url.enum.ts
+++ bittorrent-simulator/src/app/constants/urls/client-url.enum.ts
@@ -1,9 +1,9 @@
 export enum ClientUrl {
   Client1 = 'http://localhost:6001',
   Client2 = 'http://localhost:6002',
-  Client3 = 'http://localhost:6003'
-  // Client4 = 'http://localhost:6004',
-  // Client5 = 'http://localhost:6005',
+  Client3 = 'http://localhost:6003',
+  Client4 = 'http://localhost:6004',
+  // Client5 = 'http://localhost:6005'
   // Client6 = 'http://localhost:6006',
   // Client7 = 'http://localhost:6007',
   // Client8 = 'http://localhost:6008',
